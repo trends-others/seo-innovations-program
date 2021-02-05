9856ef8d5af7ec8319c6fe31bd68af8214559cd9
John Lester Almenanza <jlalmenanza@trends.com.ph>
2021-02-05 09:48:19 +0800

    Initial Commit

---
+++ puzzle.py
@@ -0,0 +1,93 @@
+
+
+
+#Start the program 
+
+answer = input("Play game? press y to continue")
+
+#Complete this to start
+if answer "y": 
+
+#Check yourindentations
+print("Started...")
+    puzzle = [{
+    "part1" : {
+        "word1" : "Welcome",
+        "word2" : "to",
+        "word3" : "SEO",
+        "word4" : "Innovations",
+        "word5" : "Program."
+    }
+    }]
+    # Turn the values into list
+    # Turn then join  the values to make complete the sentence.
+    # Output: Welcome to SEO Innovations Program.
+    
+   
+
+
+nested_list = [
+    {
+        "brand": "Cisco",
+        "model": "Catalyst 9600 Series 6-slot",
+        "description": "State-of-the-art high availability with N+1 power and fan redundancy ",
+        "ip_address":"192.168.1.1",
+        "status" : 1,
+        "random" : "Thank "
+    },
+    {
+        "brand": "Alcatel Lucent",
+        "model": "OmniSwitch 6450",
+        "description": "Stackable Gigabit Ethernet LAN Switch",
+        "ip_address":"192.168.1.2",
+        "status" : 1,
+        "random" : ["you"]
+    },
+    {
+        "brand": "Ruckus",
+        "model": "Ruckus R750",
+        "description": "8 Stream AP with wi-Fi 6(802.11ax)",
+        "ip_address":"192.168.1.3",
+        "status" : 0,
+        "random" : "for "
+    },
+    {
+        "brand": "Ruckus",
+        "model": "Ruckus R750",
+        "description": "8 Stream AP with wi-Fi 6(802.11ax)",
+        "ip_address":"192.168.1.4",
+        "status" : 1,
+        "random" : "attending "
+    },
+    {
+        "brand": "Alcatel Lucent",
+        "model": "OmniSwitch 6450",
+        "description": "Stackable Gigabit Ethernet LAN Switch",
+        "ip_address":"192.168.1.5",
+        "status" : 0,
+        "random" : ["Seo","Innovations"]
+
+    },
+     {
+        "brand": "Alcatel Lucent",
+        "model": "OmniSwitch 6450",
+        "description": "Stackable Gigabit Ethernet LAN Switch",
+        "ip_address":"192.168.1.6",
+        "status" : 1,
+        "random" : "Programm "
+
+    },
+]
+
+
+    #2 Select  Ip address and display their status from puzzle
+    # Output:
+    # 192.168.1.1 status is up
+    # 192.168.1.2 status is up
+    # 192.168.1.3 status is down
+    # 192.168.1.4 status is up
+    # 192.168.1.5 status is down
+    # 192.168.1.6 status is up
+
+    #3 Turn all values from random into a sentence.
+    #Output: Thank  you for  attending  Seo Innovations Programm
